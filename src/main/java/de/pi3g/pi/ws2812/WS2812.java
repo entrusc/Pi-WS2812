31727bc244f196d62052e9d7cbc7894a5509f0d3
Florian Frankenberger <ff@pi3g.com>
2015-11-04 17:17:43 +0100

    + okay, defaults are not okay in JNA interfaces ...

--- src/main/java/de/pi3g/pi/ws2812/WS2812.java
+++ src/main/java/de/pi3g/pi/ws2812/WS2812.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-
 package de.pi3g.pi.ws2812;
 
 import com.sun.jna.Library;
@@ -31,50 +30,149 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * A simple Java wrapper around the WS2812 library by
- * Pimoroni (https://github.com/pimoroni/unicorn-hat).
+ * A simple Java wrapper around the WS2812 library by Pimoroni
+ * (https://github.com/pimoroni/unicorn-hat).
  * <p/>
- * You need to connect the first WS2812 LED on GPIO
- * port #18 (PWM) for this to work.
+ * You need to connect the first WS2812 LED on GPIO port #18 (PWM) for this to
+ * work.
  * <p/>
- * And no at the moment it is not possible to do the
- * same with Pi4j as it does not allow using the
- * GPIO #18's PWM serialization feature.
+ * And no at the moment it is not possible to do the same with Pi4j as it does
+ * not allow using the GPIO #18's PWM serialization feature.
  *
  * @author Florian Frankenberger
  */
 public final class WS2812 {
 
-    private static WS2812Interface INSTANCE;
+    private static WS2812 INSTANCE;
+    private final WS2812Interface iface;
 
-    public static class Color_t extends Structure {
+    private WS2812(WS2812Interface iface) {
+        this.iface = iface;
+    }
+
+    /**
+     * initializes the led array with the given number of pixels (=LEDs)
+     *
+     * @param numPixels
+     */
+    public void init(int numPixels) {
+        iface.init(numPixels);
+    }
+
+    /**
+     * clears all LEDs by setting them to color (0, 0, 0)
+     */
+    public void clear() {
+        iface.clear();
+    }
+
+    /**
+     * changes the brightness of all LEDs. [0 to 1.0], where 0.0 is basically
+     * off and 1.0 being the brightest setting.
+     *
+     * @param brightness
+     */
+    public void setBrightness(float brightness) {
+        iface.setBrightness(brightness);
+    }
+
+    /**
+     * sets the color of a specific LED
+     *
+     * @param pixel
+     * @param r
+     * @param g
+     * @param b
+     */
+    public void setPixelColor(int pixel, byte r, byte g, byte b) {
+        iface.setPixelColor(pixel, r, g, b);
+    }
+
+    public  void setPixelColor(int pixel, Color color) {
+        this.setPixelColor(pixel, (byte) color.getRed(), (byte) color.getGreen(), (byte) color.getBlue());
+    }
+
+    public void fill(byte r, byte g, byte b) {
+        for (int i = 0; i < numPixels(); ++i) {
+            this.setPixelColor(i, r, g, b);
+        }
+    }
+
+    public void fill(Color color) {
+        byte r = (byte) color.getRed();
+        byte g = (byte) color.getGreen();
+        byte b = (byte) color.getBlue();
+
+        fill(r, g, b);
+    }
+
+    /**
+     * returns the current color of the given pixel/LED
+     * @param index
+     * @return
+     */
+    public Color getPixelColor(int index) {
+        Color_t color = iface.getPixelColor(index);
+        return new Color(color.r & 0xFF, color.g & 0xFF, color.b & 0xFF);
+    }
+
+    /**
+     * actually propergates the changes made to the pixels to the real LEDs.
+     * This needs to be called everytime when something is changed to actually
+     * make it visible.
+     */
+    public void show() {
+        iface.show();
+    }
+
+    /**
+     * returns the amount of pixels set in init()
+     *
+     * @see #init(int)
+     * @return
+     */
+    public int numPixels() {
+        return iface.numPixels();
+    }
+
+    /**
+     * Should be called when exiting the app. This is done automatically when
+     * the virtual machine exists via shutdown hook.
+     */
+    public void terminate() {
+        iface.terminate(0);
+    }
+
+    private static class Color_t extends Structure {
+
         public byte r;
         public byte g;
         public byte b;
 
         @Override
         protected List<String> getFieldOrder() {
-            return Arrays.asList(new String[] {
+            return Arrays.asList(new String[]{
                 "r", "g", "b"
             });
         }
 
     }
 
-    public static synchronized WS2812Interface get() {
+    public static synchronized WS2812 get() {
         if (INSTANCE == null) {
             final File tmpLibraryFile = unpackLibrary();
 
-            INSTANCE = (WS2812Interface) Native.loadLibrary(tmpLibraryFile.getAbsolutePath(), WS2812Interface.class);
+            final WS2812Interface iface = (WS2812Interface) Native.loadLibrary(tmpLibraryFile.getAbsolutePath(), WS2812Interface.class);
+            INSTANCE = new WS2812(iface);
 
             //make sure we clean up before that app exits
             Runtime.getRuntime().addShutdownHook(new Thread() {
 
                 @Override
                 public void run() {
-                    INSTANCE.clear();
-                    INSTANCE.show();
-                    INSTANCE.terminate();
+                    iface.clear();
+                    iface.show();
+                    iface.terminate(0);
 
                     Native.unregister(WS2812Interface.class);
                     tmpLibraryFile.delete();
@@ -86,7 +184,7 @@ public final class WS2812 {
         return INSTANCE;
     }
 
-    public static interface WS2812Interface extends Library {
+    private static interface WS2812Interface extends Library {
 
         /**
          * initializes the led array with the given number of pixels (=LEDs)
@@ -101,8 +199,8 @@ public final class WS2812 {
         void clear();
 
         /**
-         * changes the brightness of all LEDs. [0 to 1.0], where
-         * 0.0 is basically off and 1.0 being the brightest setting.
+         * changes the brightness of all LEDs. [0 to 1.0], where 0.0 is
+         * basically off and 1.0 being the brightest setting.
          *
          * @param brightness
          * @return
@@ -120,49 +218,26 @@ public final class WS2812 {
          */
         byte setPixelColor(int pixel, byte r, byte g, byte b);
 
-        default void setPixelColor(int pixel, Color color) {
-           this.setPixelColor(pixel, (byte) color.getRed(), (byte) color.getGreen(), (byte) color.getBlue());
-        }
-
-        default void fill(byte r, byte g, byte b) {
-            for (int i = 0; i < numPixels(); ++i) {
-                this.setPixelColor(i, r, g, b);
-            }
-        }
-
-        default void fill(Color color) {
-            byte r = (byte) color.getRed();
-            byte g = (byte) color.getGreen();
-            byte b = (byte) color.getBlue();
-
-            fill(r, g, b);
-        }
+        Color_t getPixelColor(int index);
 
         /**
-         * actually propergates the changes made to the pixels to the
-         * real LEDs. This needs to be called everytime when something
-         * is changed to actually make it visible.
+         * actually propergates the changes made to the pixels to the real LEDs.
+         * This needs to be called everytime when something is changed to
+         * actually make it visible.
          */
         void show();
 
         /**
          * returns the amount of pixels set in init()
+         *
          * @see #init(int)
          * @return
          */
         int numPixels();
 
         /**
-         * Should be called when exiting the app. This is done automatically when
-         * the virtual machine exists via shutdown hook.
-         */
-        default void terminate() {
-            terminate(0);
-        }
-
-        /**
-         * Should be called when exiting the app. This is done automatically when
-         * the virtual machine exists via shutdown hook.
+         * Should be called when exiting the app. This is done automatically
+         * when the virtual machine exists via shutdown hook.
          *
          * @param dummy
          */
@@ -181,6 +256,7 @@ public final class WS2812 {
     }
 
     private static final String LIBRARY_FILE_NAME = "libws2812-RPi.so";
+
     private static File unpackLibrary() {
         try {
             final File tmpDirectory = Files.createTempDirectory("library").toFile();
@@ -193,8 +269,7 @@ public final class WS2812 {
             byte[] buffer = new byte[512];
             try (
                     InputStream libraryIn = WS2812.class.getResourceAsStream("/" + LIBRARY_FILE_NAME);
-                    OutputStream tmpFileOut = new FileOutputStream(tmpLibraryFile)
-            ) {
+                    OutputStream tmpFileOut = new FileOutputStream(tmpLibraryFile)) {
                 int read = 0;
                 while ((read = libraryIn.read(buffer)) >= 0) {
                     tmpFileOut.write(buffer, 0, read);
